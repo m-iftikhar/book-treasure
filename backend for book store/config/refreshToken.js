9dab1c668b8717e80d78bd90b448e747cf52e27f
m-iftikhar <iftikharrj2001@gmail.com>
2024-09-28 22:35:29 +0500

    chnge in congig

--- backend for book store/config/refreshToken.js
+++ backend for book store/config/refreshToken.js
@@ -1,18 +1,12 @@
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 
-<<<<<<< HEAD
 // Load environment variables from .env file
 dotenv.config();
 
 const generateRefreshToken = (id) => {
     // Use the secret key from process.env
     let token = jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "3d" });
-=======
-const generateRefreshToken = (id)=>{
-    let token = jwt.sign({id}, " ", {expiresIn: "3d"});
->>>>>>> d591e1788d7ddb773b93170c0ce05beaab090391
-    return token;
 }
 
 export default generateRefreshToken;
